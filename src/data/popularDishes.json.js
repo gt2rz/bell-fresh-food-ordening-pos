fe8e7bcc9992b7ca738ec619f90780668e90be4b
Miguel Gutierrez <gt2rz.dev@gmail.com>
2023-10-21 17:15:28 -0500

    update popular dishes

---
+++ src/data/popularDishes.json.js
@@ -0,0 +1,42 @@
+export const popularDishes = [
+    {
+      id: 1,
+      category: "Salads",
+      name: "Fresh and Health Salad",
+      price: 2.65,
+      calories: 60,
+      persons: 4,
+      image:
+        "/images/salad_img.png",
+    },
+    {
+      id: 2,
+      category: "Salads",
+      name: "Cashew Nut Chicken Salad",
+      price: 2.65,
+      calories: 60,
+      persons: 4,
+      image:
+      "/images/salad_img.png",
+    },
+    {
+      id: 3,
+      category: "Salads",
+      name: "Cashew Nut Chicken Salad",
+      price: 2.65,
+      calories: 60,
+      persons: 4,
+      image:
+      "/images/salad_img.png",
+    },
+    {
+      id: 4,
+      category: "Salads",
+      name: "Sesame Dresing Salad",
+      price: 2.65,
+      calories: 60,
+      persons: 4,
+      image:
+      "/images/salad_img.png",
+    },
+  ];
